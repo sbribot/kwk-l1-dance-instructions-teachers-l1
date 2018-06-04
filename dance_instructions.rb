9da28f43c439ce2d11c588b311c47d489c1ccf87
Sara Beth Ribot <sbribot@gmail.com>
2018-06-04 22:23:25 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -1,2 +1,13 @@
 
 # Code your methods below
+def starting_stance
+  puts "Plant legs far apart, bend knees slightly and keep posture loose"
+end 
+
+def base_footwork
+  puts "Lift right foot"
+  puts "Return right foor to the ground"
+  #finishing with a small skip step backward ??? 
+end 
+
+def skip_step 
